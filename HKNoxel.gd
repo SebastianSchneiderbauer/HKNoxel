3155c0d1309d155240a72da712fbcd948f23cc0c
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2026-08-05 23:31:54 +0200

    add first testings

--- HKNoxel.gd
+++ HKNoxel.gd
@@ -1,8 +1,22 @@
 @tool
 @icon("res://addons/HKNoxel/icon.png")
-class_name Noxel
+class_name NoxelMap
 extends Node3D
 
 @export var cell_size: float = 1.5
-@export var collision_mask: int = 1
-@export var save_path: String = "res://sound_data/"
+@export_flags_3d_physics var wall_collision_mask: int = 1
+@export var saved_data: String = "res://sound_data/"
+
+func bake_sound_grid() -> void:
+	var start := -1.5
+	var end := 1.5
+	var step := 0.1
+	var steps := int(round((end - start) / step))
+	
+	for i in steps + 1:
+		var value := start + i * step
+		_indexOf(Vector3(value,0,0))
+
+func _indexOf(position : Vector3):
+	var test = roundf(position.x / cell_size)
+	print(str(position.x) + ": " + str(test))
--- plugin.gd
+++ plugin.gd
@@ -1,8 +1,30 @@
 @tool
 extends EditorPlugin
 
+var toolbar_button: Button
+var current_noxel: NoxelMap
+
 func _enter_tree() -> void:
-	pass
+	toolbar_button = Button.new()
+	toolbar_button.text = "Bake Sound Grid"
+	toolbar_button.pressed.connect(_on_bake_pressed)
+	add_control_to_container(CONTAINER_SPATIAL_EDITOR_MENU, toolbar_button)
+	toolbar_button.hide()
 
 func _exit_tree() -> void:
-	pass
+	remove_control_from_container(CONTAINER_SPATIAL_EDITOR_MENU, toolbar_button)
+	toolbar_button.queue_free()
+
+func _handles(object: Object) -> bool:
+	var result := object is NoxelMap
+	return result
+
+func _edit(object: Object) -> void:
+	current_noxel = object as NoxelMap
+
+func _make_visible(visible: bool) -> void:
+	toolbar_button.visible = visible
+
+func _on_bake_pressed() -> void:
+	if current_noxel:
+		current_noxel.bake_sound_grid()
