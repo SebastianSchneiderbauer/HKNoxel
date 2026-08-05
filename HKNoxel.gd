e172deb73513aae9571d5f436710d0aba973dd5d
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2026-08-05 21:34:37 +0200

    Add addon files

---
+++ HKNoxel.gd
@@ -0,0 +1,8 @@
+@tool
+@icon("res://addons/HKNoxel/icon.png")
+class_name Noxel
+extends Node3D
+
+@export var cell_size: float = 1.5
+@export var collision_mask: int = 1
+@export var save_path: String = "res://sound_data/"
