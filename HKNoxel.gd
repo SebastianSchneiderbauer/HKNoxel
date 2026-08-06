2f5762cdccc3a2c1943566f6343b21bbead86366
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2026-08-06 12:47:31 +0200

    add docs

--- HKNoxel.gd
+++ HKNoxel.gd
@@ -3,22 +3,29 @@
 class_name NoxelMap
 extends Node3D
 
+## The size of the sound voxels.
 @export var cell_size: float = 1.5
+## What the baking process sees as walls. Its recommended to have seperate layers for decoration and actual walls, so props dont block Noxels
 @export_flags_3d_physics var wall_collision_mask: int = 1
+## The node that defines the bounding box of the map. Merges its childrens AABBs, so make sure there are actual providers as children.
 @export var AABBProvider: Node3D
-func bake_sound_grid() -> void:
-	if AABBProvider:
-		dimensions = get_node_aabb(AABBProvider)
-		print(dimensions)
-	else:
-		printerr("no AABBProvider was provided")
 
-var dimensions: AABB
+var vGridStartPosition: Vector3
+var vGridDimensions: Vector3i # the dimensions of our voxelgrid
+func bake_sound_grid() -> void:
+	if not AABBProvider:
+		printerr("no AABBProvider was provided")
+	var dimensions : AABB = get_node_aabb(AABBProvider)
+	vGridStartPosition = dimensions.position
+	vGridDimensions = (dimensions.size / cell_size).ceil()
+
 func _indexOf(position : Vector3):
-	if AABBProvider:
-		dimensions = get_node_aabb(AABBProvider)
-	else:
-		return
+	if not vGridDimensions:
+		printerr("cannot get index if dimensions were not specified")
+	
+	var relPosition: Vector3 = position - vGridStartPosition
+	
+	return
 	var test = roundf(position.x / cell_size)
 	print(str(position.x) + ": " + str(test))
 
