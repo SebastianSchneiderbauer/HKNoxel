c7ddaef80e127e5555a3919b7bb7d2e30181f469
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2026-08-06 12:18:49 +0200

    add in aabb getter for the total map

--- HKNoxel.gd
+++ HKNoxel.gd
@@ -5,18 +5,42 @@ extends Node3D
 
 @export var cell_size: float = 1.5
 @export_flags_3d_physics var wall_collision_mask: int = 1
-@export var saved_data: String = "res://sound_data/"
-
+@export var AABBProvider: Node3D
 func bake_sound_grid() -> void:
-	var start := -1.5
-	var end := 1.5
-	var step := 0.1
-	var steps := int(round((end - start) / step))
-	
-	for i in steps + 1:
-		var value := start + i * step
-		_indexOf(Vector3(value,0,0))
+	if AABBProvider:
+		dimensions = get_node_aabb(AABBProvider)
+		print(dimensions)
+	else:
+		printerr("no AABBProvider was provided")
 
+var dimensions: AABB
 func _indexOf(position : Vector3):
+	if AABBProvider:
+		dimensions = get_node_aabb(AABBProvider)
+	else:
+		return
 	var test = roundf(position.x / cell_size)
 	print(str(position.x) + ": " + str(test))
+
+# partly ported from a github issue, but modified
+func get_node_aabb(node: Node3D, ignore_top_level: bool = true) -> AABB:
+	var box: AABB
+	var has_box := false
+	
+	if node is VisualInstance3D:
+		var vi := node as VisualInstance3D
+		box = vi.global_transform * vi.get_aabb()
+		has_box = true
+	
+	for i: int in node.get_child_count():
+		var child := node.get_child(i) as Node3D
+		if child and not (ignore_top_level and child.top_level):
+			var child_box := get_node_aabb(child, ignore_top_level)
+			if child_box.size != Vector3.ZERO or child_box.position != Vector3.ZERO:
+				if has_box:
+					box = box.merge(child_box)
+				else:
+					box = child_box
+					has_box = true
+	
+	return box
