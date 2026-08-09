5575ff12f2ce357cb3f6ac802ca44ac3031dc2d6
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2026-08-09 14:01:24 +0200

    update reset method so all relevant info is ported over

--- HKNoxelManager.gd
+++ HKNoxelManager.gd
@@ -19,9 +19,22 @@ func removeCurrentNmap():
 var walls: NoxelWallStorage
 var soundLevel: PackedByteArray
 var emitter: PackedByteArray
+var dimensions: Vector3i
+
+var gridStartPosition: Vector3
+var cellCount: int
+var cellSize: float
+
 func _resetMaps():
 	walls = currentNoxelMap.wallBakeData
-	var dim : Vector3i = currentNoxelMap.vGridDimensions
-	var count := dim.x * dim.y * dim.z
-	soundLevel.resize(count)
-	emitter.resize(count)
+	dimensions = currentNoxelMap.vGridDimensions
+	cellCount = dimensions.x * dimensions.y * dimensions.z
+	soundLevel.resize(cellCount)
+	emitter.resize(cellCount)
+	gridStartPosition = currentNoxelMap.vGridStartPosition
+	cellSize = currentNoxelMap.cell_size
+
+func _indexOf(objectPosition: Vector3) -> int: # ported from the Noxel
+	var relativePosition: Vector3 = objectPosition - gridStartPosition
+	var voxelPosition: Vector3 = (relativePosition / cellSize).round()
+	return int(voxelPosition.x + voxelPosition.y * dimensions.x + voxelPosition.z * dimensions.x * dimensions.y)
