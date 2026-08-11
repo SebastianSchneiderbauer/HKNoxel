1bc7a8cfb6fd40ae60c3870e3e33ff36dc5a96ec
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2026-08-12 00:34:04 +0200

    first prototype of sound propagation

--- HKNoxelManager.gd
+++ HKNoxelManager.gd
@@ -61,7 +61,7 @@ func get_source(id: int) -> Node:
 		return null
 	return _active_sources[id]
 
-func _getNeighbourIndexes(cellIndex: int) -> Array[int]: # this is a abomination and should be banished into the dephs of hell
+func _getFreeNeighbourIndexes(cellIndex: int) -> Array[int]: # this is a abomination and should be banished into the dephs of hell
 	# formula x + y * dim.x + z * dim.x * dim.y
 	var result : Array[int]
 	# x
@@ -69,11 +69,13 @@ func _getNeighbourIndexes(cellIndex: int) -> Array[int]: # this is a abomination
 		result.append(cellIndex + 1)
 	if _checkCellIndex(cellIndex - 1):
 		result.append(cellIndex - 1)
+	
 	# y
 	if _checkCellIndex(cellIndex + 1 * dimensions.x):
 		result.append(cellIndex + 1 * dimensions.x)
 	if _checkCellIndex(cellIndex - 1 * dimensions.x):
 		result.append(cellIndex - 1 * dimensions.x)
+	
 	# z
 	if _checkCellIndex(cellIndex + 1 * dimensions.x * dimensions.y):
 		result.append(cellIndex + 1 * dimensions.x * dimensions.y)
@@ -82,7 +84,7 @@ func _getNeighbourIndexes(cellIndex: int) -> Array[int]: # this is a abomination
 	
 	return result
 func _checkCellIndex(cellIndex: int) -> bool:
-	return cellIndex >= 0 and cellIndex < cellCount
+	return cellIndex >= 0 and cellIndex < cellCount and not walls.isWall(cellIndex)
 ## Used for emitting a sound at a position. Decibels is a integer that can take values storable in an unsigned byte. Fails if the emitterId is not valid or the position is outside of the baked NoxelMap
 func emitSound(startPosition: Vector3, decibels: int, emitterId: int):
 #region safety checks
@@ -94,8 +96,8 @@ func emitSound(startPosition: Vector3, decibels: int, emitterId: int):
 		printerr("emitterID not registered")
 		return
 	var cellIndex := _indexOf(startPosition)
-	if cellIndex >= cellCount:
-		printerr("sound is out of this world") # holy
+	if cellIndex < 0 or cellIndex >= cellCount:
+		printerr("sound is out of this world") # holy wording
 		return
 	if walls.isWall(cellIndex):
 		printerr("sound cannot be started in wall")
@@ -115,3 +117,25 @@ func emitSound(startPosition: Vector3, decibels: int, emitterId: int):
 	soundLevel[cellIndex] = decibels
 	emitter[cellIndex] = emitterId
 	activeCellIds.append(cellIndex)
+
+const CONFINEMENT_DEDUCTION : float = 0.4
+const CONSTANT_DEDICTION_MULTIPLIER : float = 0.95 
+func _physics_process(delta: float) -> void:
+	# we currently would only reach a sound-speed of 60 m/s, nowhere close to the 343 m/s of the actual speed of sound (also dependent on the cellsize)
+	# 2 options, either ignore it or simulate multiple spreads per tick (5.7 btw, fuck, just make it 6 atp.)
+	if Input.is_action_just_pressed("ctrl"):
+		simulateSound()
+func simulateSound() -> void:
+	for cellID in activeCellIds:
+		var emitterID = emitter[cellID]
+		var neighbourCellIDs = _getFreeNeighbourIndexes(cellID)
+		var freeNeighbourCount = neighbourCellIDs.size()
+		var currentSoundLevel = soundLevel[cellID]
+		var newSoundLevel = currentSoundLevel - (float(freeNeighbourCount) / 6) * CONFINEMENT_DEDUCTION
+		for neighbourCellID in neighbourCellIDs:
+			var neighbourSoundLevel = soundLevel[neighbourCellID]
+			if newSoundLevel <= neighbourSoundLevel:
+				continue
+			
+			emitter[neighbourCellID] = emitterID
+			soundLevel[neighbourCellID] = newSoundLevel
